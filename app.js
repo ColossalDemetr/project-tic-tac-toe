7d9e1dc73253f90dc9bc6243733cd12deeaadb3f
dev-colossal <241526608+ColossalDemetr@users.noreply.github.com>
2026-08-20 03:52:39 +0300

    Add GameController module with makeATurn, switchPlayer, checkWin

--- app.js
+++ app.js
@@ -36,3 +36,29 @@ function createPlayer (name, marker) {
     
     return player;
 };
+
+const gameController = (() => {
+
+    const players = [
+        createPlayer("Player1", "X"),
+        createPlayer("Player2", "O")
+    ];
+
+    let currentPlayer = players[0];
+
+    function makeATurn() {
+
+    };
+
+    function switchPlayer() {
+
+    };
+
+    function checkWin() {
+
+    };
+
+
+    return { makeATurn, switchPlayer, checkWin };
+
+})();
