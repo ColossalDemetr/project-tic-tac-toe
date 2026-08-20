967dcf5fced31128736b4074d16a0bd786a69984
dev-colossal <241526608+ColossalDemetr@users.noreply.github.com>
2026-08-20 04:49:47 +0300

    Add game logic: makeATurn, switchPlayer, checkWin

--- app.js
+++ app.js
@@ -46,15 +46,34 @@ const gameController = (() => {
 
     let currentPlayer = players[0];
 
-    function makeATurn() {
-
+    function makeATurn(index) {
+        gameBoard.placeMarker(index, currentPlayer.marker);
+        switchPlayer();
     };
 
     function switchPlayer() {
-
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
     };
 
     function checkWin() {
+
+        const winConditions = [
+            [0,1,2], [3,4,5], [6,7,8],
+            [0,3,6], [1,4,7], [2,5,8],
+            [0,4,8], [2,4,6]
+        ];
+
+        const board = gameBoard.getBoard();
+        
+
+
+        return winConditions.some(combination => {
+            
+            return (board[combination[0]] === board[combination[1]] &&
+            board[combination[1]] === board[combination[2]] &&
+            board[combination[0]] !== "");
+           
+        });
 
     };
 
